dac357fdd37dfc80fe7829a8aa68f6078e59313b
webzille <153772404+webzille@users.noreply.github.com>
2024-05-16 22:13:43 -0500

    Update searchDemo.php

--- searchDemo.php
+++ searchDemo.php
@@ -13,7 +13,7 @@ $nodes = $parser->parse()->getNodes();
 $search = new Search($nodes);
 
 // Test cases
-$search->resetResults()->find([
+$search->clearResults()->find([
     ['type' => 'property', 'property' => 'width', 'value' => '80%']
 ]);
 echo "Results for property 'width' with value '80%':\n\n";
@@ -21,7 +21,7 @@ foreach ($search->results() as $results) {
     echo "<strong>$results->property: {$results->getChildren()[0]->value};</strong> on Line: $results->lineNo" . PHP_EOL;
 }
 
-$search->resetResults()->find([
+$search->clearResults()->find([
     ['type' => 'selector', 'value' => 'h1']
 ]);
 echo "\n\n\nResults for selector 'h1':\n\n";
@@ -29,7 +29,7 @@ foreach ($search->results() as $results) {
     echo "<strong>$results->selector</strong> on Line: $results->lineNo" . PHP_EOL;
 }
 
-$search->resetResults()->find([
+$search->clearResults()->find([
     ['type' => 'id', 'value' => 'my-id']
 ]);
 echo "\n\n\nResults for ID 'my-id':\n\n";
@@ -37,7 +37,7 @@ foreach ($search->results() as $results) {
     echo "<strong>$results->selector</strong> on Line: $results->lineNo" . PHP_EOL;
 }
 
-$search->resetResults()->find([
+$search->clearResults()->find([
     ['type' => 'attribute', 'value' => 'href']
 ]);
 echo "\n\n\nResults for attribute 'href':\n\n";
@@ -45,7 +45,7 @@ foreach ($search->results() as $results) {
     echo "<strong>$results->selector</strong> on Line: $results->lineNo" . PHP_EOL;
 }
 
-$search->resetResults()->find([
+$search->clearResults()->find([
     ['type' => 'media', 'value' => 'screen and (min-width: 768px)']
 ]);
 echo "\n\n\nResults for media query 'screen and (min-width: 768px)':\n\n";
@@ -53,7 +53,7 @@ foreach ($search->results() as $results) {
     echo "<strong>" . trim("$results->rule $results->params") . "</strong> on Line: $results->lineNo" . PHP_EOL;
 }
 
-$search->resetResults()->find([
+$search->clearResults()->find([
     ['type' => 'media', 'value' => '768px']
 ]);
 echo "\n\n\nResults for media query '768px':\n\n";
@@ -61,7 +61,7 @@ foreach ($search->results() as $results) {
     echo "<strong>" . trim("$results->rule $results->params") . "</strong> on Line: $results->lineNo" . PHP_EOL;
 }
 
-$search->resetResults()->find([
+$search->clearResults()->find([
     ['type' => 'pseudo', 'value' => 'hover']
 ]);
 echo "\n\n\nResults for pseudo-class 'hover':\n\n";
@@ -69,7 +69,7 @@ foreach ($search->results() as $results) {
     echo "<strong>$results->selector</strong> on Line: $results->lineNo" . PHP_EOL;
 }
 
-$search->resetResults()->find([
+$search->clearResults()->find([
     ['type' => 'type', 'value' => AtRule::class]
 ]);
 echo "\n\n\nResults for type 'AtRule':\n\n";
@@ -77,7 +77,7 @@ foreach ($search->results() as $results) {
     echo "<strong>" . trim("$results->rule $results->params") . "</strong> on Line: $results->lineNo" . PHP_EOL;
 }
 
-$search->resetResults()->find([
+$search->clearResults()->find([
     ['type' => 'class', 'value' => 'container']
 ]);
 echo "\n\n\nResults for class 'container':\n\n";
