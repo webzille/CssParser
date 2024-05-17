e8cf02d432f4a1af86bad4a330eea30997d47df1
webzille <153772404+webzille@users.noreply.github.com>
2024-05-16 22:51:20 -0500

    Update demo.php

--- demo.php
+++ demo.php
@@ -3,6 +3,7 @@
 use Webzille\CssParser\CssFormat;
 use Webzille\CssParser\Parser;
 use Webzille\CssParser\Render;
+use Webzille\CssParser\Util\Optimize;
 
 require "vendor/autoload.php";
 
@@ -10,7 +11,17 @@ $minified = false;
 $parser = new Parser("stylesheet.css");
 $nodes = $parser->parse()->getNodes();
 $format = (new CssFormat)->setIndent("    ")->setNewLine("\n");
-$render = new Render($nodes, $format);
+
+$optimized = Optimize::optimize($nodes)
+                        //->removeWhitespace()
+                        ->removeDuplicates()
+                        ->toShorthand()
+                        ->optimizeColors()
+                        ->vendorPrefix();
+
+$render = new Render($optimized->getNodes(), $format);
+
 $css = trim($render->css());
 //print_r($nodes);
+print_r($optimized->getModified());
 echo "<textarea style='width: 100%; height: 100%;'>$css</textarea>";
